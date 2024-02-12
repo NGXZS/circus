feb10f71bcec42a507260157493b733109b89357
ngxzs <seanng01@outlook.com>
2024-02-12 22:36:09 +0800

    Add all changes done in lecture

---
+++ src/main/java/circus/animal/Animal.java
@@ -0,0 +1,9 @@
+package circus.animal;
+
+import circus.Asset;
+
+public abstract class Animal implements Asset { // Add abstract coz concept, no actual Animal object
+
+    public abstract String speak(); // coz abstract method no need method body
+
+}
